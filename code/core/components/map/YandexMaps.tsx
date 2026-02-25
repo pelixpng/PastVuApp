5ca9b91d326987e753967aa5d9fd0462fa8a2c0b
pelix.png <semeonky@gmail.com>
2026-02-25 22:07:01 +0100

    fix: maps

--- code/core/components/map/YandexMaps.tsx
+++ code/core/components/map/YandexMaps.tsx
@@ -1,5 +1,13 @@
 import { YaMap, Marker } from 'react-native-yamap-lite'
-import { StyleSheet, View, LayoutChangeEvent } from 'react-native'
+import { StyleSheet, useWindowDimensions } from 'react-native'
+import { RefObject, useRef } from 'react'
+import { useTheme } from '@react-navigation/native'
+import { Region } from 'react-native-maps'
+import { MapMarker } from '../../types/apiPhotoList'
+import { getZoom } from '../../utils/getMapData'
+import { yamapRef } from '../../../mobile/screens/home/map/Map.vm'
+
+const TILE_SIZE = 256
 
 type CameraPosition = {
   nativeEvent: {
@@ -7,12 +15,6 @@ type CameraPosition = {
     zoom: number
   }
 }
-import { RefObject, useRef, useState } from 'react'
-import { useTheme } from '@react-navigation/native'
-import { Region } from 'react-native-maps'
-import { MapMarker } from '../../types/apiPhotoList'
-import { getZoom } from '../../utils/getMapData'
-import { yamapRef } from '../../../mobile/screens/home/map/Map.vm'
 
 type Props = {
   mapRef: RefObject<any>
@@ -32,8 +34,7 @@ export const YandexMaps = ({
   onGoToLocation,
 }: Props) => {
   const { names } = useTheme()
-  const [lockedHeight, setLockedHeight] = useState(0)
-  const lockedHeightRef = useRef(0)
+  const { width: screenWidth } = useWindowDimensions()
 
   const yamapInitialRegion = useRef({
     lat: initialRegion.latitude,
@@ -43,64 +44,54 @@ export const YandexMaps = ({
 
   const handleCameraChange = (e: CameraPosition) => {
     const { point, zoom } = e.nativeEvent
-    const latitudeDelta = 360 / Math.pow(2, zoom)
+    const degreePerTile = 360 / Math.pow(2, zoom)
+    const longitudeDelta = degreePerTile * (screenWidth / TILE_SIZE)
+    const latitudeDelta = longitudeDelta
     onRegionChangeComplete({
       latitude: point.lat,
       longitude: point.lon,
       latitudeDelta,
-      longitudeDelta: latitudeDelta,
+      longitudeDelta,
     })
   }
 
-  const onLayout = (e: LayoutChangeEvent) => {
-    const h = e.nativeEvent.layout.height
-    if (h > 0) {
-      if (h > lockedHeightRef.current) {
-        lockedHeightRef.current = h
-        setLockedHeight(h)
-      }
-    }
-  }
-
   return (
-    <View style={lockedHeight > 0 ? { height: lockedHeight } : s.flexOne} onLayout={onLayout}>
-      <YaMap
-        ref={yamapRef}
-        style={s.flexOne}
-        nightMode={names.themeName === 'dark'}
-        showUserPosition={false}
-        minZoomPreference={4}
-        initialRegion={yamapInitialRegion}
-        onCameraPositionChangeEnd={handleCameraChange}>
-        {markers.map((marker, index) => {
-          if (marker._type === 'photo') {
-            return (
-              <Marker
-                key={index}
-                rotated
-                rotation={marker.dir}
-                point={{ lat: marker.location.latitude, lon: marker.location.longitude }}
-                source={{
-                  uri: names.themeName === 'light' ? marker.marker[0] : marker.marker[1],
-                }}
-                onPress={() => onShowPhoto(marker.cid, marker.title)}
-              />
-            )
-          } else {
-            return (
-              <Marker
-                key={index}
-                point={{ lat: marker.location.latitude, lon: marker.location.longitude }}
-                source={{
-                  uri: names.themeName === 'light' ? marker.marker[0] : marker.marker[1],
-                }}
-                onPress={() => onGoToLocation(marker.location.latitude, marker.location.longitude)}
-              />
-            )
-          }
-        })}
-      </YaMap>
-    </View>
+    <YaMap
+      ref={yamapRef}
+      style={s.flexOne}
+      nightMode={names.themeName === 'dark'}
+      showUserPosition={false}
+      minZoomPreference={4}
+      initialRegion={yamapInitialRegion}
+      onCameraPositionChangeEnd={handleCameraChange}>
+      {markers.map((marker, index) => {
+        if (marker._type === 'photo') {
+          return (
+            <Marker
+              key={index}
+              rotated
+              rotation={marker.dir}
+              point={{ lat: marker.location.latitude, lon: marker.location.longitude }}
+              source={{
+                uri: names.themeName === 'light' ? marker.marker[0] : marker.marker[1],
+              }}
+              onPress={() => onShowPhoto(marker.cid, marker.title)}
+            />
+          )
+        } else {
+          return (
+            <Marker
+              key={index}
+              point={{ lat: marker.location.latitude, lon: marker.location.longitude }}
+              source={{
+                uri: names.themeName === 'light' ? marker.marker[0] : marker.marker[1],
+              }}
+              onPress={() => onGoToLocation(marker.location.latitude, marker.location.longitude)}
+            />
+          )
+        }
+      })}
+    </YaMap>
   )
 }
 
