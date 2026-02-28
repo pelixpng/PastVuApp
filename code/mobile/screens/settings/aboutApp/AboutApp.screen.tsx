2f2ea297e62801de79e38021ca70e6ad392a4768
pelix.png <semeonky@gmail.com>
2026-02-28 22:49:38 +0100

    feat: up version

--- code/mobile/screens/settings/aboutApp/AboutApp.screen.tsx
+++ code/mobile/screens/settings/aboutApp/AboutApp.screen.tsx
@@ -15,7 +15,7 @@ export const AboutAppScreen = () => {
         <Image style={s.image} source={require('../../../../assets/icon.png')} />
         <Spacer height={16} />
         <Text selectable style={[s.descriptionText, { color: colors.textSecond }]}>
-          Версия 2.4.0 от 20 февраля 2026 г.
+          Версия 2.5.0 от 28 февраля 2026 г.
         </Text>
       </View>
       <Spacer height={24} />
